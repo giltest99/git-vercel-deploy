b4144a81bf3d5f75199b833cfb7000e7cae45dd7
Gilles <sellig.blater@free.fr>
2025-05-13 18:02:30 +0200

    Misc fix

--- src/app/typicode/page.tsx
+++ src/app/typicode/page.tsx
@@ -9,13 +9,8 @@ export default async function page() {
       <h1 className="text-3xl my-6">Liste des posts</h1>
       <ul className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mb-8">
         {postsData.map((post: { id: number; title: string }) => (
-          <li key={post.id} className="rounded shadow">
-            {/* <Link
-              href={`/typicode/${post.id}`}
-              className="inline-block h-full w-full p-4 text-center bg-neutral-200 hover:bg-neutral-300 transition-colors duration-200"
-            >
-              {post.title}
-            </Link> */}
+          <li key={post.id} className="rounded shadow w-full p-4 bg-white">
+            {post.title}
           </li>
         ))}
       </ul>
