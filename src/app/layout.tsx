edbade46cd884a00ca75cb5828fad7972f37d351
Gilles <sellig.blater@free.fr>
2025-05-13 22:57:41 +0200

    Inter font sans

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,20 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { /* Geist, */ Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import Navigation from "@/components/Navigation";
 
-const geistSans = Geist({
+/* const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-});
+}); */
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const inter = Inter({
+  variable: "--font-inter-sans",
   subsets: ["latin"],
 });
 
@@ -26,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col w-full h-screen`}
+        className={`${inter.variable} ${geistMono.variable} ${inter.variable} antialiased flex flex-col w-full h-screen`}
       >
         <header className="bg-neutral-300 flex justify-center">
           <Navigation />
